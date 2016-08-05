607e51d57d62d04780f3404ccce702ecba19bb52
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-08-05 15:26:11 +0900

    Add plugin file

---
+++ qa-plugin.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+	Plugin Name: YS Badges
+	Plugin URI:
+	Plugin Description: Provides simple badges.
+	Plugin Version: 1.0
+	Plugin Date: 2016-08-05
+	Plugin Author: 38qa.net
+	Plugin Author URI: http://38qa.net/
+	Plugin License: GPLv2
+	Plugin Minimum Question2Answer Version: 1.7
+	Plugin Update Check URI:
+*/
+
+if (!defined('QA_VERSION')) { // don't allow this page to be requested directly from browser
+	header('Location: ../../');
+	exit;
+}
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
