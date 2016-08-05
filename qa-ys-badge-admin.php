92a0bc99984c5b03358830cd2c84355ddfcd85c6
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-08-05 15:53:54 +0900

    Add default options

--- qa-ys-badge-admin.php
+++ qa-ys-badge-admin.php
@@ -3,6 +3,27 @@
 
 class qa_ys_badge_admin
 {
+	function allow_template($tamplate)
+	{
+		return ($template !== 'admin');
+	}
+
+	function option_default($option)
+	{
+		$badges = qa_get_badge_list();
+		
+		$slug = preg_replace('/ys_badge_(.+)_[^_]+/',"$1",$option);
+		switch($opstion) {
+			case 'ys_badge_'.$slug.'_name'：
+				return qa_lang('badges/'.$slug);
+			case 'ys_badge_'.$slug.'_var':
+				return @$badges[$slug]['var'];
+			case 'ys_badge_'.$slug.'_enabled':
+				return '0';
+			default:
+				return null;
+		}
+	}
 }
 
 /*
