894da4ce2d572ce233dcc39531a918b204e8868a
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-08-08 15:34:24 +0900

    Add create table functions

--- qa-ys-badge-admin.php
+++ qa-ys-badge-admin.php
@@ -151,30 +151,8 @@ class qa_ys_badge_admin
 
 			if (qa_opt('ys_badge_active')) {
 
-				qa_db_query_sub(
-					'CREATE TABLE IF NOT EXISTS ^ys_userbadges ('.
-						'id INT(11) NOT NULL AUTO_INCREMENT,'.
-						'awarded_at DATETIME NOT NULL,'.
-						'user_id INT(11) NOT NULL,'.
-						'notify TINYINT DEFAULT 0 NOT NULL,'.
-						'object_id INT(10),'.
-						'badge_slug VARCHAR (64) CHARACTER SET ascii DEFAULT \'\','.
-						'PRIMARY KEY (id)'.
-					') ENGINE=InnoDB DEFAULT CHARSET=utf8'
-				);
-
-				qa_db_query_sub(
-					'CREATE TABLE IF NOT EXISTS ^ys_achievements ('.
-						'user_id INT(11) UNIQUE NOT NULL,'.
-						'first_visit DATETIME,'.
-						'oldest_consec_visit DATETIME,'.
-						'longest_consec_visit INT(10),'.
-						'last_visit DATETIME,'.
-						'total_days_visited INT(10),'.
-						'questions_read INT(10),'.
-						'posts_edited INT(10)'.
-					') ENGINE=InnoDB DEFAULT CHARSET=utf8'
-				);
+				ys_badge_db::create_userbadges_table();
+				ys_badge_db::create_achievements_table();
 
 				// set badge names, vars and states
 
--- ys-badge-db.php
+++ ys-badge-db.php
@@ -7,7 +7,36 @@ if (!defined('QA_VERSION')) { // don't allow this page to be requested directly 
 
 class ys_badge_db
 {
+	public static function create_userbadges_table()
+	{
+		qa_db_query_sub(
+			'CREATE TABLE IF NOT EXISTS ^ys_userbadges ('.
+				'id INT(11) NOT NULL AUTO_INCREMENT,'.
+				'awarded_at DATETIME NOT NULL,'.
+				'user_id INT(11) NOT NULL,'.
+				'notify TINYINT DEFAULT 0 NOT NULL,'.
+				'object_id INT(10),'.
+				'badge_slug VARCHAR (64) CHARACTER SET ascii DEFAULT \'\','.
+				'PRIMARY KEY (id)'.
+			') ENGINE=InnoDB DEFAULT CHARSET=utf8'
+		);
+	}
 
+	public static function create_achievements_table()
+	{		
+		qa_db_query_sub(
+			'CREATE TABLE IF NOT EXISTS ^ys_achievements ('.
+				'user_id INT(11) UNIQUE NOT NULL,'.
+				'first_visit DATETIME,'.
+				'oldest_consec_visit DATETIME,'.
+				'longest_consec_visit INT(10),'.
+				'last_visit DATETIME,'.
+				'total_days_visited INT(10),'.
+				'questions_read INT(10),'.
+				'posts_edited INT(10)'.
+			') ENGINE=InnoDB DEFAULT CHARSET=utf8'
+		);
+	}
 }
 
 /*
